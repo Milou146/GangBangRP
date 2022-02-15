82268f966964c460f6e3421fa4bdb197a0a9d778
Milou146 <57072450+Milou146@users.noreply.github.com>
2022-02-15 11:48:09 +0100

    c_arms des yakuzas

---
+++ addons/playermodels/lua/autorun/kimonos.lua
@@ -0,0 +1,89 @@
+player_manager.AddValidModel("Kimonos", "models/players/Kimonos.mdl")
+player_manager.AddValidHands("Kimonos", "models/weapons/kimonos/c_arms.mdl", 0, 0)
+
+player_manager.AddValidModel("Kimonos_02", "models/players/Kimonos_02.mdl")
+player_manager.AddValidHands("Kimonos_02", "models/weapons/kimonos/c_arms.mdl", 1, 0)
+
+player_manager.AddValidModel("Kimonos_03", "models/players/Kimonos_03.mdl")
+player_manager.AddValidHands("Kimonos_03", "models/weapons/kimonos/c_arms.mdl", 2, 0)
+
+player_manager.AddValidModel("Kimonos_04", "models/players/Kimonos_04.mdl")
+player_manager.AddValidHands("Kimonos_04", "models/weapons/kimonos/c_arms.mdl", 3, 0)
+
+player_manager.AddValidModel("Kimonos_05", "models/players/Kimonos_05.mdl")
+player_manager.AddValidHands("Kimonos_05", "models/weapons/kimonos/c_arms.mdl", 4, 0)
+
+player_manager.AddValidModel("Kimonos_06", "models/players/Kimonos_06.mdl")
+player_manager.AddValidHands("Kimonos_06", "models/weapons/kimonos/c_arms.mdl", 5, 0)
+
+player_manager.AddValidModel("Kimonos_07", "models/players/Kimonos_07.mdl")
+player_manager.AddValidHands("Kimonos_07", "models/weapons/kimonos/c_arms.mdl", 6, 0)
+
+player_manager.AddValidModel("Kimonos_08", "models/players/Kimonos_08.mdl")
+player_manager.AddValidHands("Kimonos_08", "models/weapons/kimonos/c_arms.mdl", 7, 0)
+
+player_manager.AddValidModel("Kimonos_09", "models/players/Kimonos_09.mdl")
+player_manager.AddValidHands("Kimonos_09", "models/weapons/kimonos/c_arms.mdl", 8, 0)
+
+player_manager.AddValidModel("Kimonos_10", "models/players/Kimonos_10.mdl")
+player_manager.AddValidHands("Kimonos_10", "models/weapons/kimonos/c_arms.mdl", 9, 0)
+
+player_manager.AddValidModel("Kimonos_11", "models/players/Kimonos_11.mdl")
+player_manager.AddValidHands("Kimonos_11", "models/weapons/kimonos/c_arms.mdl", 10, 0)
+
+player_manager.AddValidModel("Kimonos_12", "models/players/Kimonos_12.mdl")
+player_manager.AddValidHands("Kimonos_12", "models/weapons/kimonos/c_arms.mdl", 11, 0)
+
+player_manager.AddValidModel("Kimonos_13", "models/players/Kimonos_13.mdl")
+player_manager.AddValidHands("Kimonos_13", "models/weapons/kimonos/c_arms.mdl", 12, 0)
+
+player_manager.AddValidModel("Kimonos_14", "models/players/Kimonos_14.mdl")
+player_manager.AddValidHands("Kimonos_14", "models/weapons/kimonos/c_arms.mdl", 13, 0)
+
+player_manager.AddValidModel("Kimonos_15", "models/players/Kimonos_15.mdl")
+player_manager.AddValidHands("Kimonos_15", "models/weapons/kimonos/c_arms.mdl", 14, 0)
+
+player_manager.AddValidModel("Kimonos_16", "models/players/Kimonos_16.mdl")
+player_manager.AddValidHands("Kimonos_16", "models/weapons/kimonos/c_arms.mdl", 15, 0)
+
+player_manager.AddValidModel("Kimonos_17", "models/players/Kimonos_17.mdl")
+player_manager.AddValidHands("Kimonos_17", "models/weapons/kimonos/c_arms.mdl", 16, 0)
+
+player_manager.AddValidModel("Kimonos_18", "models/players/Kimonos_18.mdl")
+player_manager.AddValidHands("Kimonos_18", "models/weapons/kimonos/c_arms.mdl", 17, 0)
+
+player_manager.AddValidModel("Kimonos_19", "models/players/Kimonos_19.mdl")
+player_manager.AddValidHands("Kimonos_19", "models/weapons/kimonos/c_arms.mdl", 18, 0)
+
+player_manager.AddValidModel("Kimonos_20", "models/players/Kimonos_20.mdl")
+player_manager.AddValidHands("Kimonos_20", "models/weapons/kimonos/c_arms.mdl", 19, 0)
+
+player_manager.AddValidModel("Kimonos_21", "models/players/Kimonos_21.mdl")
+player_manager.AddValidHands("Kimonos_21", "models/weapons/kimonos/c_arms.mdl", 20, 0)
+
+player_manager.AddValidModel("Kimonos_22", "models/players/Kimonos_22.mdl")
+player_manager.AddValidHands("Kimonos_22", "models/weapons/kimonos/c_arms.mdl", 21, 0)
+
+player_manager.AddValidModel("Kimonos_23", "models/players/Kimonos_23.mdl")
+player_manager.AddValidHands("Kimonos_23", "models/weapons/kimonos/c_arms.mdl", 22, 0)
+
+player_manager.AddValidModel("Kimonos_24", "models/players/Kimonos_24.mdl")
+player_manager.AddValidHands("Kimonos_24", "models/weapons/kimonos/c_arms.mdl", 23, 0)
+
+player_manager.AddValidModel("Kimonos_25", "models/players/Kimonos_25.mdl")
+player_manager.AddValidHands("Kimonos_25", "models/weapons/kimonos/c_arms.mdl", 24, 0)
+
+player_manager.AddValidModel("Kimonos_26", "models/players/Kimonos_26.mdl")
+player_manager.AddValidHands("Kimonos_26", "models/weapons/kimonos/c_arms.mdl", 25, 0)
+
+player_manager.AddValidModel("Kimonos_27", "models/players/Kimonos_27.mdl")
+player_manager.AddValidHands("Kimonos_27", "models/weapons/kimonos/c_arms.mdl", 26, 0)
+
+player_manager.AddValidModel("Kimonos_28", "models/players/Kimonos_28.mdl")
+player_manager.AddValidHands("Kimonos_28", "models/weapons/kimonos/c_arms.mdl", 27, 0)
+
+player_manager.AddValidModel("Kimonos_29", "models/players/Kimonos_29.mdl")
+player_manager.AddValidHands("Kimonos_29", "models/weapons/kimonos/c_arms.mdl", 28, 0)
+
+player_manager.AddValidModel("Kimonos_30", "models/players/Kimonos_30.mdl")
+player_manager.AddValidHands("Kimonos_30", "models/weapons/kimonos/c_arms.mdl", 29, 0)
